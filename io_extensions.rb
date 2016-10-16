d6813a2af6596f75358032b7c8f61873a31d272a
Daniel Steele <danielsteele@hotmail.co.uk>
2016-10-16 20:27:47 +0100

    You can now answer with the first letter in a selection

--- io_extensions.rb
+++ io_extensions.rb
@@ -8,12 +8,12 @@ module IOExtensions
 
   def say(sentance, options = nil, color: :white)
     puts
-    sleep 0.5 unless $debug
+    sleep 0.65 unless $debug
     puts sentance.colorize(color)
     puts options.join("  ") if options
   end
 
-  def ask(type, question, selection=nil)
+  def ask(type, question, selection = nil)
     say question, selection
     return random_answer(type, question, selection) if $debug
     ans = gets.strip.chomp.downcase
@@ -22,19 +22,25 @@ module IOExtensions
     return ans if type == :text
   end
 
-  def random_answer(type, question, selection=nil)
+  def random_answer(type, question, selection = nil)
     return [true, false].sample if type == :bool
     return selection.sample.to_sym if type == :select
     return ("A".."Z").to_a.shuffle.take(6).join if type == :text
   end
 
   def selection_answer(question, ans, selection)
-    if selection.include?(ans) || selection.map(&:to_s).include?(ans)
-      return ans.to_sym
-    else
-      puts "That doesn't sound right, try again"
-      ask(:select, question, selection)
-    end
+    return ans.to_sym if selection.map(&:to_s).include?(ans)
+    return ans_is_first_letter?(ans, selection)
+    puts "That doesn't sound right, try again"
+    ask(:select, question, selection)
+  end
+
+  def ans_is_first_letter?(ans, selection)
+    selection.map(&:to_s).any? { |s| s.start_with? ans } ? ans_to_selection(ans, selection) : false
+  end
+
+  def ans_to_selection(ans, selection)
+    selection.select { |s| s.start_with? ans }.first.to_sym
   end
 
   def positive_answer?(ans)
